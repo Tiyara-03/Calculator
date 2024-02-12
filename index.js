ec250617f09084c21cfcefebca30bbc887584287
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-13 01:54:25 +0530

    starting with js

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const App = express();
 
-App.use(bodyParser.urlencoded({extend:true}));
+App.use(bodyParser.urlencoded({extended:true}));
 
 App.get("/",(req ,res)=> res.sendFile(__dirname + "/index.html"));
 
 App.post("/",(req, res)=>{
-    var n1 = Number(req.body.n1);
-    var n2 = Number(req.body.n2);
-    var result = n1+ n2;
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
     res.send("The answer is " + result);
+
 
 });
 
-App.listen(3000, ()=>console.timeLog("The server started sucessfully on port 3000"));
+App.listen(80, ()=>console.log("The server started sucessfully on server 80"));
