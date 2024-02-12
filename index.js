a7c900fa9ab0878f0d9346a25fa3a77d1d193ff7
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-13 02:16:25 +0530

    On Process

--- index.js
+++ index.js
@@ -9,8 +9,19 @@ App.get("/",(req ,res)=> res.sendFile(__dirname + "/index.html"));
 App.post("/",(req, res)=>{
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    var result = num1 + num2;
-    res.send("The answer is " + result);
+    
+    function Add(){
+        // let Add = document.getElementById('Add') ;
+        var result = num1+num2;
+        res.send("The answer is " +result);
+    }
+
+    function sub(){
+
+        // let sub = document.getElementById("sub");
+        var result = num1-num2;
+        res.send("The answer is " + result);
+    }
 
 
 });
