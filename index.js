61fad9f61f173b6fd987aefbb98559fe4fe8c728
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-13 21:30:14 +0530

    In Process With Html

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ App.get("/",(req ,res)=> res.sendFile(__dirname + "/index.html"));
 App.post("/",(req, res)=>{
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    
+    var result;
+
     function Add(){
-        // let Add = document.getElementById('Add') ;
-        var result = num1+num2;
-        res.send("The answer is " +result);
+
+         result = num1+num2;
     }
 
-    function sub(){
+    function Sub(){
 
-        // let sub = document.getElementById("sub");
-        var result = num1-num2;
-        res.send("The answer is " + result);
+         result = num1 - num2;
     }
+
+    Add(); Sub();
+    res.send("The answer is " + result);
+    
+
 
 
 });
