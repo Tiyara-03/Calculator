565b7399df6f242ecb2254cb28c372548c11171a
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-21 12:56:24 +0530

    Javascript practices

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const App = express();
+const path = require("path");
 
 App.use(bodyparser.urlencoded({extended:true}));
+
+// App.use(express.static('public'));
+App.use(express.static(path.join(__dirname, 'public')));
+// App.use(express.urlencoded());
 
 
 App.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"));
